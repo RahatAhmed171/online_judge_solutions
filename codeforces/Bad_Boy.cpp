afbb144bb313780d9434ef92b4a5b280a3477692
RahatAhmed171 <69518859+RahatAhmed171@users.noreply.github.com>
2023-09-03 01:29:58 +0600

    Bad_Boy(900)

---
+++ codeforces/Bad_Boy.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int t;
+int n;
+int m;
+int i;
+int j;
+
+
+cin>>t;
+for(int p=0;p<t;p++)
+{
+cin>>n;
+cin>>m;
+cin>>i;
+cin>>j;
+cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+
+}
+
+}
+    
