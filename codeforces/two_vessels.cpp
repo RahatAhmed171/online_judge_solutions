e1db0e9d567cba3b122c8c53a7e55ac66b374a70
RahatAhmed171 <69518859+RahatAhmed171@users.noreply.github.com>
2023-09-19 20:41:02 +0600

    Update two_vessels

--- codeforces/two_vessels.cpp
+++ codeforces/two_vessels.cpp
@@ -1,1 +1,26 @@
-t
+
+
+#include <iostream>
+#include<cmath>
+ 
+using namespace std;
+int main(){
+int t;
+float a,b,c;
+int move=0;
+float diff;
+float half;
+cin>>t;
+for(int i=0;i<t;i++)
+{
+cin>>a;
+cin>>b;
+cin>>c;
+diff=abs(a-b);
+half=diff/2;
+
+move=ceil(half/c);
+cout<<move<<endl;
+
+ }
+ }
