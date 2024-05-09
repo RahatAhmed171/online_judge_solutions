54156ff41012761edda5816cbcf44d7fff3866d7
RahatAhmed171 <69518859+RahatAhmed171@users.noreply.github.com>
2024-05-09 16:36:07 +0600

    Game_with_integers.cpp(800)

---
+++ codeforces/Game_with_integers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include<map>
+using namespace std;
+
+
+int main() {
+
+int t;
+int n;
+
+
+cin>>t;
+for(int i=0;i<t;i++)
+{
+
+cin>>n;
+if(n%3==0)
+{
+cout<<"Second"<<endl;
+}
+else{
+cout<<"First"<<endl;
+}
+
+};
+ } 
